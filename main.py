052ff71c9992d527cea1dd1fc349b5427e2d08d3
Cyber-M0rn <116242781+Cyber-M0rn@users.noreply.github.com>
2023-03-23 10:43:10 +0700

    finish position 1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from core.database import Base, engine
 
 from modules.sample.controller import router as sample_router
+from modules.position.controller import router as position_router
 
 app = FastAPI()
 
@@ -14,6 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(sample_router)
+app.include_router(position_router)
 
 
 @app.on_event("startup")
